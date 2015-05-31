f6353c2d91c8516802e8f5ea9fae76e4b8d6e1a0
Nate Pauzenga <npauzenga@gmail.com>
2015-05-31 15:21:00 -0400

    working solution for Caesar

--- pset2/caesar.c
+++ pset2/caesar.c
@@ -2,41 +2,58 @@
 #include <string.h>
 #include <stdlib.h>
 #include <cs50.h>
+#include <ctype.h>
 
 // encrypt a user-proved string via a key provided by parameter
 
 int main(int argc, string argv[])
 {
+    // get the key parameter and convert to int
+    int key = atoi(argv[1]);
+    int alphaIndex;
+    char cipherText;
+    
     if (argc != 2)
     {
         printf("Please run again with one key argument\n");
         return 1;
     }
-
-    // get the key parameter and convert to int
-    int key = atoi(argv[1]);
-        
+   
     string userString = GetString();
         
     // iterate over the userString, adding key to each char
-    // and storing in new string
-    for (int i = 0, int n = strlen(userString); i < n; i++)
+    // and printing
+    for (int i = 0, n = strlen(userString); i < n; i++)
     {
         // check if the char is a letter, then determine case
         if (isalpha(userString[i]))
         {
             if (isupper(userString[i]))
             {
-                
+                // letter - 'A' = alpha index (0)
+                alphaIndex = userString[i] - 'A';
+                cipherText = ((alphaIndex + key) % 26) + 'A'; 
+                printf("%c", cipherText);
             }
             else if (islower(userString[i]))
             {
-            
-            }
+                // letter - 'a' = alpha index (0)
+                alphaIndex = userString[i] - 'a';
+                cipherText = ((alphaIndex + key) % 26) + 'a'; 
+                printf("%c", cipherText);
+            }   
         }
+        else
+        {
+            printf("%c", userString[i]);
+        } 
         
     }
+    
+    printf("\n");
        
     return 0;
-
 }
+
+char encypher()
+
